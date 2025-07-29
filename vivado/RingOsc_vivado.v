3d75f8fad90a347dd1edaa29f05160c5d78b31c9
vracton <sonit.sahoo@icloud.com>
2025-07-29 18:51:26 +0300

    basys 3 conversion

--- vivado/RingOsc_vivado.v
+++ vivado/RingOsc_vivado.v
@@ -1,19 +1,27 @@
 module RingOsc #(parameter CW = 4) ( //number of oscillators should be odd and equals CW+1
-    (*keep*) input RESET,
+    (* KEEP = "true", DONT_TOUCH = "true" *) input RESET,
     output RAW_ENTROPY_OUT
 );
 
-	(*keep*) wire [CW:0] chain;
+	(* KEEP = "true", DONT_TOUCH = "true" *) wire [CW:0] chain;
 	
 	genvar i;
     generate
-        for (i=1; i <= CW; i=i+1) 
-        begin: not_seq
-            assign chain[i] = !chain[i-1];
+        for (i=1; i <= CW; i=i+1) begin: not_seq
+            (* KEEP = "true", DONT_TOUCH = "true" *)
+            LUT1 #(.INIT(2'b01)) inverter (
+                .I0(chain[i-1]),
+                .O(chain[i])
+            );
         end
     endgenerate
 	
-	assign chain[0] = !(chain[CW]|RESET);
+	(* KEEP = "true", DONT_TOUCH = "true" *)
+    LUT2 #(.INIT(4'b0001)) feedback (
+        .I0(chain[CW]),
+        .I1(RESET),
+        .O(chain[0])
+    );
 
 	assign RAW_ENTROPY_OUT = chain[0]; 	
 endmodule
--- vivado/trng_unit_vivado.v
+++ vivado/trng_unit_vivado.v
@@ -4,11 +4,12 @@ module TRNG (
     input wire raw_entropy_in, // This is the raw entropy input
     input wire trng_request,
     output reg [31:0] random_number,
-    output wire ready
+    output reg ready
 );
 
     // TRNG implementation goes here
     wire ring_oscillator_output;
+    
     RingOsc #(.CW(4)) ring_oscillator (
         .RESET(~rst_n),
         .RAW_ENTROPY_OUT(ring_oscillator_output)
@@ -27,16 +28,16 @@ module TRNG (
             ready <= 1'b0;
         end else begin
             if (trng_request & ~ready) begin
-            shift_reg <= {shift_reg[30:0], ring_oscillator_output}; //The curly brackets are used to concatenate the bits
-            bit_count <= bit_count + 1;
-            if (bit_count == 31) begin
-                random_number <= {shift_reg[30:0],ring_oscillator_output}; // Output the collected bits when we have 32 bits
-                ready <= 1'b1; // Indicate that the random number is ready
-            end
+                shift_reg <= {shift_reg[30:0], ring_oscillator_output};
+                bit_count <= bit_count + 1;
+                if (bit_count == 31) begin
+                    random_number <= {shift_reg[30:0], ring_oscillator_output};
+                    ready <= 1'b1;
+                end
             end else if (~trng_request) begin
-                ready <= 1'b0; // Reset ready signal after outputting the random number
-                bit_count <= 6'b0; // Reset bit count
-                shift_reg <= 32'b0; // Reset shift register
+                ready <= 1'b0;
+                bit_count <= 6'b0;
+                shift_reg <= 32'b0;
             end
         end
     end
